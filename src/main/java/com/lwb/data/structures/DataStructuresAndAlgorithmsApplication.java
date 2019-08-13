83c0d795f3e48a0b2dbba80da3500bc8fef36664
liuweibo <liuwb@youxuepai.com>
2019-08-13 15:05:36 +0800

    【刘维柏】项目初始化

---
+++ src/main/java/com/lwb/data/structures/DataStructuresAndAlgorithmsApplication.java
@@ -0,0 +1,17 @@
+package com.lwb.data.structures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author liuweibo
+ */
+@SpringBootApplication
+public class DataStructuresAndAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresAndAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lwb/data/structures/tree/BTree.java
@@ -0,0 +1,12 @@
+package com.lwb.data.structures.tree;
+
+/**
+ * B树
+ * @author liuweibo
+ * @date 2019/8/13
+ */
+public class BTree {
+
+
+
+}
